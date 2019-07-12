461fbc3529fd77b7f281575b52d94cc8eeeee74f
Tomáš Molinari <im@toomeenoo.ml>
2019-07-12 16:41:10 +0200

    Update checker.py

--- ci-tools/checker.py
+++ ci-tools/checker.py
@@ -1,4 +1,4 @@
 import sys, glob, os
-for filename in glob.glob(filename = sys.argv[1]+"/*.py"):
+for filename in glob.glob(sys.argv[1]+"/*.py"):
     source = open(filename, 'r').read() + '\n'
     compile(source, filename, 'exec')
