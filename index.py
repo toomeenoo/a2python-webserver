1e68df6f4c29b0e6c6a0de83b3e8bf6851fcf6b2
Tomáš Molinari <tomie.molinari@gmail.com>
2019-07-13 20:03:10 +0200

    Content of example test file

--- index.py
+++ index.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python3
+﻿#!/usr/bin/env python3
 import sys, os
 from weblib import *
 w = Weblib()
-w.write("(Document-start)")
+w.write("<!DOCTYPE html><head><title>Pythonwebserver</title></head><body>")
 a = w.server("REQUEST_SCHEME")+"//"+w.server("SERVER_NAME")+w.server("REQUEST_URI")
-w.write(a+"<br>")
+w.write("URL: "+a+"<br>")
 
-w.write(str(w.get()))
-w.write(str(w.post()))
+w.write("Get: "+str(w.get())+"<br>")
+w.write("Post: "+str(w.post())+"<br>")
+w.write("Cookie \"test\" should be set to: \"testcookie-value\", valid next 4hrs <br>")
 
-w.write("<br><br>Dir<br>")
+w.write("<br><br><b>Dir</b> variables<br>")
 for name in dir():
     myvalue = eval(name)
-    w.write( name+" => "+str(myvalue)+"<br>")
+    w.write( name+" =&gt; "+str(myvalue).replace("&","&amp;").replace("<","&lt;").replace(">","&rt;")+"<br>")
 
-w.write("<br>Apache<br>")
+w.write("<br><b>Apache</b> variables<br>")
 for name, value in os.environ.items():
-    w.write(name+" => "+value+"<br>")
+    w.write(name+" =&gt; "+value+"<br>")
 
-w.setcookie("test", "75301", 60*60*4)
-w.write("<br>//end")
+w.setcookie("test", "testcookie-value", 60*60*4)
+w.write("</body>")
